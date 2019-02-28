013f5a21caca166c7b79a9997c2d4780cc039152
Vladyslav FIL <vfil@e1r4p8.unit.ua>
2019-03-01 00:11:02 +0200

    must be fixed

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def print_photos(photos):
 
 
 def print_slides(slides):
-    print "Slides:"
+
     for s in slides:
         print "ids:", s.ids, "count:", s.count, "tags:", s.tags
     print "-----"
@@ -56,23 +56,61 @@ def get_unique(tags):
             unique_list.append(x)
     return unique_list
 
-# def search_best_slide(photos):
-#     if not photos[1]:
-#         return
-#     first = photos.pop[0]
-#     stags = similar_tags_count(first.tags, photos[0].tags)
-#     second_best = {"idx": 0, "res": stags}
-#
-#     for i in range(1, len(photos)):
-#         stags = unique_tags_count(first.tags, photos[i].tags)
-#         if second_best["res"] < stags:
-#             second_best["res"] = stags
-#             second_best["idx"] = i
+
+def compare(object1, object2):
+ equal = 0
+ equal_tags = []
+ diff1 = 0
+ diff2 = 0
+ for i in object1.tags:
+  changed = False
+  for j in object2.tags:
+   if i == j:
+    equal = equal + 1
+    changed = True
+    equal_tags.append(i)
+    break
+  if not changed:
+   diff1 = diff1 + 1
+
+ for i in object2.tags:
+  if not (i in equal_tags):
+   found = False
+   for j in object1.tags:
+    if j == i:
+     found = True
+     break
+   if not found:
+    diff2 = diff2 + 1
+
+ values = [equal, diff1, diff2]
+ return min(values)
+
+
+
+def choose_by_tags(slides_array):
+
+ result_array = []
+ for i in range(len(slides_array) - 1):
+  maximum = 0
+  obj1_index = 0
+  obj2_index = 0
+  for j in range(i + 1, len(slides_array)):
+   min_value_tags = compare(slides_array[i], slides_array[j])
+   if (min_value_tags > maximum):
+    maximum = min_value_tags
+    obj1_index = i
+    obj2_index = j
+  if (maximum > 0):
+   result_array.append(slides_array[obj1_index])
+   result_array.append(slides_array[obj2_index])
+   slides_array.pop(obj2_index)
+ return(result_array)
 
 
 def search_best_slide(photos):
     for i in range(len(photos) - 1):
-        if not photos[i + 1]:
+        if i + 1 >= len(photos) - 1:
             break
         min = similar_tags_count(photos[i].tags, photos[i + 1].tags)
         obj1_index = i
@@ -83,9 +121,9 @@ def search_best_slide(photos):
                 min = min_value_tags
                 obj1_index = i
                 obj2_index = j
-        print obj1_index, obj2_index
         slides.append(Slide([photos[obj1_index], photos[obj2_index]]))
         photos.pop(obj2_index)
+
 
 
 
@@ -105,11 +143,19 @@ if len(sys.argv) > 1:
             else:
                 photos.append(obj)
 
-    # test = Slide([photos[0], photos[1]])
     sort_by_count(photos)
 
     search_best_slide(photos)
     sort_by_count(slides)
 
-    print_photos(photos)
-    print_slides(slides)
+    res_slides = choose_by_tags(slides)
+    # print_slides(res_slides)
+
+    f = open(inputName + ".res", "w")
+    f.write(str(len(res_slides)) + '\n')
+    for i in res_slides:
+        if len(i.ids) == 2:
+            f.write(str(i.ids[0]) + " " + str(i.ids[1]) + '\n')
+        else:
+            f.write(str(i.ids[0]) + '\n')
+
